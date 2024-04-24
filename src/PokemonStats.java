1d2c5b5a3333bbbdf1e9ddd53038113899207844
Isabelle Wang <ilwang@smith.edu>
2024-04-24 15:23:21 -0400

    starting

--- src/PokemonStats.java
+++ src/PokemonStats.java
@@ -1,4 +1,12 @@
 public class PokemonStats {
-    
+    /**
+     * Number of nodes in the graph
+     * 
+     * @return int number of nodes
+     */
+    public static int numNodes() {
+        return -1;
+
+    }
 
 }
